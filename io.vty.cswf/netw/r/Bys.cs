fedecb3edd5dfb2f0109f3bc46bd692d3b822163
Centny <centny@gmail.com>
2015-07-25 12:25:11 +0800

    first import cswf project.

---
+++ io.vty.cswf/netw/r/Bys.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace io.vty.cswf.netw.r
+{
+    /// <summary>
+    /// Bys is byte[] slice.
+    /// </summary>
+    public interface Bys
+    {
+        /// <summary>
+        /// byte[] slice length.
+        /// </summary>
+        int length { get; }
+        /// <summary>
+        /// byte[] slice offset.
+        /// </summary>
+        int offset { get; }
+
+        /// <summary>
+        /// original byte[].
+        /// </summary>
+        byte[] bys { get; }
+
+        /// <summary>
+        /// copy byte[] slice.
+        /// </summary>
+        byte[] sbys { get; }
+
+        /// <summary>
+        /// reset the slice offset and length basing orignal byte[].
+        /// </summary>
+        /// <param name="off">offset</param>
+        /// <param name="len">length</param>
+        void reset(int off, int len);
+
+        /// <summary>
+        /// forward slice by length basing current offset and length.
+        /// </summary>
+        /// <param name="len">length</param>
+        void forward(int len);
+
+        /// <summary>
+        /// create slice by offset basing current offset and length..
+        /// </summary>
+        /// <param name="off"></param>
+        /// <returns>new slice</returns>
+        Bys slice(int off);
+
+        /// <summary>
+        /// create slice by offset and length basing current offset and length.
+        /// </summary>
+        /// <param name="off">offset</param>
+        /// <param name="len">length</param>
+        /// <returns>new slice</returns>
+        Bys slice(int off, int len);
+
+        /// <summary>
+        /// getting byte by index.
+        /// </summary>
+        /// <param name="idx">index</param>
+        /// <returns>byte value</returns>
+        byte get(int idx);
+
+        /// <summary>
+        /// getting one short integer by offset basing current offset.
+        /// </summary>
+        /// <param name="off">offset</param>
+        /// <returns>short integer</returns>
+        short shortv(int off);
+
+        /// <summary>
+        /// covert byte[] to string basing current offset and length.
+        /// </summary>
+        /// <returns>string</returns>
+        String toBs();
+    }
+}
---
+++ io.vty.cswf/netw/r/NetwBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace io.vty.cswf.netw.r
+{
+    /// <summary>
+    /// the netw base interface to read and write from stream by command.
+    /// it will write data by mode head.
+    /// mode head:H_MODE|data length with 16 bit integer|data
+    /// </summary>
+    public interface NetwBase
+    {
+        public const byte[] H_MOD = {byte('s') };
+        /// <summary>
+        /// read and wait data to full byte[] by offset and length.
+        /// </summary>
+        /// <param name="buf">byte buffer</param>
+        /// <param name="off">read offset</param>
+        /// <param name="len">read data length</param>
+        /// <returns>data length</returns>
+        int readw(byte[] buf, int off, int len);
+
+
+    }
+}
