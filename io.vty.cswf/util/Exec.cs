9d848397e2a5558896a3015c0b5325527d035f9f
Centny <centny@gmail.com>
2016-02-26 16:03:03 +0800

    redirect out/err to result

--- io.vty.cswf/util/Exec.cs
+++ io.vty.cswf/util/Exec.cs
@@ -10,15 +10,33 @@ namespace io.vty.cswf.util
     {
         public static int exec(out string output, string exe, params string[] args)
         {
+            StringBuilder sb = new StringBuilder();
             Process proc = new Process();
             proc.StartInfo.UseShellExecute = false;
             proc.StartInfo.FileName = exe;
             proc.StartInfo.Arguments = string.Join(" ", args);
             proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
             proc.StartInfo.CreateNoWindow = true;
             proc.Start();
+            proc.OutputDataReceived += (sender, e) =>
+            {
+                lock (sb)
+                {
+                    sb.Append(e.Data + "\n");
+                }
+            };
+            proc.ErrorDataReceived += (sender, e) =>
+            {
+                lock (sb)
+                {
+                    sb.Append(e.Data + "\n");
+                }
+            };
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
             proc.WaitForExit();
-            output = proc.StandardOutput.ReadToEnd();
+            output = sb.ToString();
             return proc.ExitCode;
         }
     }
