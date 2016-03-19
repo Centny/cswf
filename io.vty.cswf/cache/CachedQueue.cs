f539e8e98aea618db698f291567561a372190080
Centny <centny@gmail.com>
2016-03-19 23:05:13 +0800

    adding log

--- io.vty.cswf/cache/CachedQueue.cs
+++ io.vty.cswf/cache/CachedQueue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using io.vty.cswf.log;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,36 +10,45 @@ namespace io.vty.cswf.cache
 {
     public class CachedQueue<T> : ConcurrentQueue<T>
     {
+        private static readonly ILog L = Log.New();
         protected Timer ClearTimer;
         public int MaxIdle = 0;
 
         public CachedQueue(int period = 30000, int idle = 0)
         {
             this.ClearTimer = new Timer(this.DoClear, null, period, period);
+            this.MaxIdle = idle;
         }
 
         protected void DoClear(object state)
         {
             this.Clear(this.MaxIdle);
         }
-        
+
         protected void Clear(int idle)
         {
+            var cleared = 0;
             while (this.Count > idle)
             {
                 T result;
                 if (!this.TryDequeue(out result))
                 {
-                    continue;
+                    break;
                 }
                 if (result is IDisposable)
                 {
                     (result as IDisposable).Dispose();
                 }
+                cleared += 1;
+            }
+            if (cleared > 0)
+            {
+                L.D("CachedQueue clear {0} item for timeout", cleared);
             }
         }
         public virtual void Clear()
         {
+            L.D("CachedQueue do clear all cached");
             this.Clear(0);
         }
     }
