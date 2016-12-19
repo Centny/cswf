a0fe7d0e61ff9de1330c05b153344f6b34dcd75e
Centny <centny@gmail.com>
2016-12-19 11:59:02 +0800

    adding CORS filter.

--- io.vty.cswf.test.console/Program.cs
+++ io.vty.cswf.test.console/Program.cs
@@ -1,8 +1,11 @@
 ﻿using io.vty.cswf.log;
+using io.vty.cswf.netw.http;
+using io.vty.cswf.netw.http.filter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 //using log4net;
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
@@ -12,17 +15,16 @@ namespace io.vty.cswf.test.console
     {
         static void Main(string[] args)
         {
-            //TestLog.show();
-            //ILog xx =Log.New();
-            //xx.D("xxxss");
-            /*
-            ILog log;
-            log = LogManager.GetLogger("ssss");
-            log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-            log.Error("sddfsf");
-           // log.Debug("xxxxkkk");
-            */
-            //Console.WriteLine("sdsf");
+            var srv = new Server();
+            srv.AddPrefix("http://localhost:9233/");
+            srv.AddF(".*", new CORS().Exec);
+            srv.AddH(".*", r =>
+             {
+                 r.WriteLine("abcv");
+                 return HResult.HRES_RETURN;
+             });
+            srv.Start();
+            Thread.Sleep(1000000);
         }
     }
 }
---
+++ io.vty.cswf/netw/http/filter/CORS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace io.vty.cswf.netw.http.filter
+{
+    public class CORS
+    {
+
+        public IDictionary<String, String> Sites = new Dictionary<String, String>();
+        public String[] Methods = new String[] { "GET", "POST" };
+        public String[] Headers = new String[] { "Origin", "X-Requested-With", "Content-Type", "Accept" };
+        public CORS()
+        {
+            this.Sites.Add("*", "N");
+        }
+        public CORS(String site)
+        {
+            this.Sites.Add(site, "N");
+        }
+        public virtual HResult Exec(Request r)
+        {
+            var origin = r.req.Headers.Get("Origin");
+            if (String.IsNullOrEmpty(origin))
+            {
+                return HResult.HRES_CONTINUE;
+            }
+            if (this.Sites.ContainsKey("*"))
+            {
+                return this.Allow(r, "*");
+            }
+            if (this.Sites.ContainsKey(origin))
+            {
+                return this.Allow(r, origin);
+            }
+            r.res.StatusCode = 403;
+            r.WriteLine(origin + " is not allowed by cors");
+            return HResult.HRES_RETURN;
+
+        }
+
+        protected virtual HResult Allow(Request r, String site)
+        {
+            r.res.AddHeader("Access-Control-Allow-Origin", site);
+            if (this.Headers != null && this.Headers.Length > 0)
+            {
+                r.res.AddHeader("Access-Control-Allow-Headers", String.Join(", ", this.Headers));
+            }
+            if (this.Methods != null && this.Methods.Length > 0)
+            {
+                r.res.AddHeader("Access-Control-Allow-Methods", String.Join(", ", this.Methods));
+            }
+            if (r.req.HttpMethod == "OPTIONS")
+            {
+                return HResult.HRES_RETURN;
+            }
+            else
+            {
+                return HResult.HRES_CONTINUE;
+            }
+        }
+    }
+}
